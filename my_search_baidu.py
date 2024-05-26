b3bbd63d0b2c1a33362c97363c4191cd888d8846
aino980 <13313150+aino1980@user.noreply.gitee.com>
2024-05-26 08:40:36 +0800

    第四章-第六章_百度搜索

--- my_search_baidu.py
+++ my_search_baidu.py
@@ -1,6 +1,9 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 
 def test_baidu(page: Page):
     page.goto(url="https://www.baidu.com")
-    page.wait_for_timeout(5_000)
+    # page.wait_for_timeout(5_000)
+    page.locator('//input[@name="wd"]').fill("playwright")
+    page.get_by_text("百度一下").click()
+    expect(page.get_by_text("https://github.com/microsoft/playwright")).to_be_visible()
