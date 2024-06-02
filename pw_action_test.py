574ceb51857f1e7d9aa82066a83b0708cd6ebec9
aino980 <13313150+aino1980@user.noreply.gitee.com>
2024-06-02 10:04:33 +0800

    第十章_提示消息的元素定位和断言

--- pw_action_test.py
+++ pw_action_test.py
@@ -11,3 +11,11 @@ def test_pw_click(page: Page):
     page.get_by_text("点击我试试1").click(force=True)
     page.get_by_text("点击我试试1").click(no_wait_after=True)
     page.get_by_text("点击我试试1").click(trial=True)
+    page.get_by_text("点击我试试1").dblclick()
+
+
+def test_pw_notification_message(page: Page):
+    page.goto("/demo/button")
+    page.wait_for_timeout(3_000)
+    page.get_by_text("点击我试试1").click()
+    expect(page.get_by_text("点击成功1!")).to_be_visible()
